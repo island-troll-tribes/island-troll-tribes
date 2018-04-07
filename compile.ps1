fc73c09a01455e0907a0ce398064cd884c546227
Cezary Rojewski <b4nnar@gmail.com>
2018-04-07 22:01:47 +0200

    Removal of Ruby dependancy for ITT map script compilation. (#59)
    Disclaimer:
    Objective of this change is to remove ruby dependancy while keeping code is status quo.
    
    - Implemented compile.ps1 powershell script file which goal is to complite .j and .zn files using jasshelper.
    - Updated .gitignore to account for new generated file.
    - Converted all .erb files to pure .j or .zn files.
    - Fixed file extension for TypeStructs file.
    - Moved MapOut.j outside of /init directory to keep its content at the end of generated file as it should be.
    - Added CHEATS_ENABLED global boolean for controlling behavior of CheatPack library initialization.

---
+++ compile.ps1
@@ -0,0 +1,50 @@
+﻿<#
+ Copyright (c) 2018 ITT Development team
+    Author: Cezary Rojewski
+
+.DESCRIPTION
+    Given the input params, attempts to compiles map script using jasshelper.
+
+.PARAMETER  JasshelperArgs
+    String value for additional arguments to pass to jasshelper.
+#>
+
+Param(
+    [string]
+    $JasshelperArgs  # defaults to an empty string
+)
+
+$ScriptPath  = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+$CommonJ     = Join-Path -Path $ScriptPath -ChildPath 'bin\jasshelper\common.j'
+$BlizzardJ   = Join-Path -Path $ScriptPath -ChildPath 'bin\jasshelper\blizzard.j'
+$Jasshelper  = Join-Path -Path $ScriptPath -ChildPath 'bin\jasshelper\jasshelper.exe'
+
+# Path to directory containing all .j and .zn files to be complited using jasshelper
+$JassFolder  = Join-Path -Path $ScriptPath -ChildPath 'jass'
+# Path to unparsed .j file generated prior to complitation
+$GeneratedJ  = Join-Path -Path $ScriptPath -ChildPath 'bin\generated.j'
+# Path to output .j file to be created after complitation
+$OutputJ     = Join-Path -Path $ScriptPath -ChildPath 'wurst\war3map.j'
+
+# Create GeneratedJ file from content of all .j and .zn files
+$InputFiles  = Get-ChildItem -Path $JassFolder -Include *.j, *.zn -Recurse
+New-Item $GeneratedJ -ItemType File -Force
+
+ForEach ($File in $InputFiles)
+{
+    If ($File.Extension -eq '.zn')
+    {
+        Add-Content -Path $GeneratedJ -Value "//! zinc"
+        Get-Content -Path $File | Add-Content $GeneratedJ
+        Add-Content -Path $GeneratedJ -Value "//! endzinc"
+    }
+    Else
+    {
+        Get-Content -Path $File | Add-Content $GeneratedJ
+    }
+}
+
+# Run jasshelper to perform actual complitaion of map script
+$Params = @('--scriptonly', $JasshelperArgs, $CommonJ, $BlizzardJ, $GeneratedJ, $OutputJ)
+& $Jasshelper $Params
